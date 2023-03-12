4bf90fb4ee1784926c1931a94fc8de3abcd04e67
Satish Venkatakrishnan <satish860@gmail.com>
2023-03-12 10:30:22 +0530

    Header Text added sucessfully

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -15,16 +15,22 @@ function HomeScreen({ navigation, route }) {
       headerShown: false,
     });
   }, []);
-  
+
   return (
-    <SafeAreaView style={styles.AndroidSafeArea} className="bg-white pt-5">
-      <View>
+    <SafeAreaView style={styles.AndroidSafeArea} 
+      className="bg-white pt-5">
+      <View className="flex-row space-x-2 pb-4 px-2">
         <Image
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           source={{
             uri: "https://links.papareact.com/wru",
           }}
         />
+
+        <View className="flex-1">
+          <Text className="text-gray-400 font-bold text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">Current Location</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
